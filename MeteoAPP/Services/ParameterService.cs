a8517567240abc8ce662c340e0408370c635532d
valmir09alimi@hotmail.com <valmir09alimi@hotmail.com>
2025-04-04 10:36:50 +0200

    added: parameter service for blazor pages

---
+++ MeteoAPP/Services/ParameterService.cs
@@ -0,0 +1,25 @@
+
+namespace MeteoAPP.Services
+{
+
+    public interface IParameterService
+    {
+        int GetData();
+        void SetData(int newData);
+    }
+
+    public class ParameterService : IParameterService
+    {
+        private int data = 0;
+
+        public int GetData()
+        {
+            return data;
+        }
+
+        public void SetData(int newData)
+        {
+            data = newData;
+        }
+    }
+}
